95292107bf94aa7c78e9f933bd323e9880822c89
Ido Erez <idodoerez@gmail.com>
2021-01-25 21:12:23 +0200

    ready

--- server.js
+++ server.js
@@ -5,13 +5,14 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const path = require('path')
 mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost/transactionsDB', { useNewUrlParser: true},  { useUnifiedTopology: true })
-// mongoose.connect("mongodb+srv://idodoerez:Tradingpostfinalproject135@cluster0.d2ipx.mongodb.net/transactionsDB?retryWrites=true&w=majority")
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
-    next()
-})
+mongoose.connect("mongodb+srv://idodoerez:Tradingpostfinalproject135@cluster0.d2ipx.mongodb.net/transactionsDB?retryWrites=true&w=majority")
+// app.use(function (req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*')
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
+//     next()
+// })
+
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(bodyParser.json())
 app.use(express.urlencoded({ extended: false }))
