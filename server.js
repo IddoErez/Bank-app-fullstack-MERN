0d69e0c979c7edfb86cdb2bb70bf9ecd03073bc5
Ido Erez <idodoerez@gmail.com>
2021-01-28 18:10:19 +0200

    ready for deployment

--- server.js
+++ server.js
@@ -4,16 +4,8 @@ const api = require('./server/routes/api')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const path = require('path')
-// mongoose.connect('mongodb://localhost/transactionsDB', { useNewUrlParser: true},  { useUnifiedTopology: true })
 
 mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost/transactionsDB')
-// mongoose.connect("mongodb+srv://idodoerez:Tradingpostfinalproject135@cluster0.d2ipx.mongodb.net/transactionsDB?retryWrites=true&w=majority")
-// app.use(function (req, res, next) {
-//     res.header('Access-Control-Allow-Origin', '*')
-//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
-//     next()
-// })
 
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(bodyParser.json())
